56cb3ab1a6ff476f75abcb20e7031b086b4f3cca
Francisco Irarrazabal <firarrazabal@utem.cl>
2023-12-07 00:19:51 -0300

    Menú realizado
    Botones funcionado-Script escenas y salir del juego

--- Assets/Scripts/Cambio_Escenas.cs
+++ Assets/Scripts/Cambio_Escenas.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Cambio_Escenas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        public void LoadScene (string nombreEscena)
+        {
+            SceneManager.LoadScene(nombreEscena);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
